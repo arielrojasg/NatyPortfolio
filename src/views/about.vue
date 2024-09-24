f64d9e1bffdf0877dfe750ce5b3430f4852607e9
Ariel Rojas <arojas@MacBook-Pro.local>
2024-09-23 23:10:08 -0600

    changes to about

--- src/views/about.vue
+++ src/views/about.vue
@@ -3,17 +3,36 @@
 		<div class="about-container">
 		  <h4 class="about-title">NATALIA VILLALOBOS</h4>
 		  <h1 class="about-bigger-title">BRAND DESIGNER</h1>
-		  <p class="about-text">Hi! I'm a Costa Rican brand designer! I love creating unique, eye-catching and meaningful brands!
-			Also I really enjoy creating social media content to share my work and make design more accessible and fun!
-			Other things I love are books, bubble tea and my cat Cremoleta.</p>
+		  <p class="about-text">I am an independent brand designer based in Costa Rica. I specialize in creating strategic brands, social media content, and unique websites that resonate with your audience. With a keen eye for aesthetics and a deep understanding of strategy, my expertise will take your brand from ordinary to exceptional.</p>
 		</div>
 	</div>
 	<section class="work-together-section">
 		<h2 class="work-together-title">WANT ME TO WORK TOGETHER?</h2>
 		<p class="work-together-text">If you're ready to start a new dream project, click the button</p>
 		<p class="work-together-text">below to fill my enquiry form so we can work together!</p>
-		<button class="work-together-button">I'm ready</button>
+		<button class="button">I'm ready</button>
 	</section>
+	<section class="info-section">
+  <div class="info-card half-height">
+    <h2 class="info-title">WHAT I DO</h2>
+    <p class="info-description">
+      As a brand designer, I craft unique visual identities that captivate and resonate with audiences. I create compelling logos, cohesive branding materials, and engaging visual elements that help businesses stand out and communicate their essence effectively.
+    </p>
+  </div>
+  <div class="info-card half-height">
+    <h2 class="info-title">HOW I DO IT</h2>
+    <p class="info-description">
+      I start with a deep understanding of each client’s vision and goals, followed by a thorough research and creative exploration process. My approach involves meticulous attention to detail and a commitment to delivering high-quality, tailored solutions. Through collaboration and creativity, I ensure that each brand design is both visually striking and strategically sound.
+    </p>
+  </div>
+  <div class="info-card full-height">
+    <h2 class="info-title">WHY I DO IT</h2>
+    <p class="info-description">
+      I am passionate about helping brands tell their stories through design. I believe that a well-designed brand can transform a business, build trust, and forge meaningful connections with its audience.
+    </p>
+  </div>
+</section>
+
 </template>
 <script>
 export default {
