baa65290feae2a678ffdacfc17308aebf8584406
Ariel Rojas <arojas@MacBook-Pro.local>
2024-10-12 23:47:00 -0600

    added changes to enquirenow

--- src/views/enquirenow.vue
+++ src/views/enquirenow.vue
@@ -1,38 +1,28 @@
 <template>
-        <section class="brand-impact-section">
-		<h2 class="brand-impact-small-title">READY TO BOOST YOUR</h2>
-		<h2 class="brand-impact-big-title">BRAND'S IMPACT?</h2>
-		<p class="brand-impact-text">Branding is crucial for businesses as it creates a distinct identity, builds credibility, and fosters customer</p>
-		<p class="brand-impact-text">loyalty. It communicates the values, personality, and essence of a brand, helping it stand out in a competitive</p>
-		<p class="brand-impact-text">market and forge meaningful connections with its target audience.</p>
-		<button class="brand-impact-button">I'm ready</button>
-        <div class="scrolling-text-enquire">
-            <div class="text-container-enquire">
-              <p class="scrolling-content-enquire">BOOKING FOR JULY</p>
-              <span class="separator-enquire">|</span>
-              <p class="scrolling-content-enquire">BOOKING FOR JULY</p>
-              <span class="separator-enquire">|</span>
-              <p class="scrolling-content-enquire">BOOKING FOR JULY</p>
-              <span class="separator-enquire">|</span>
-              <p class="scrolling-content-enquire">BOOKING FOR JULY</p>
-              <span class="separator-enquire">|</span>
-              <p class="scrolling-content-enquire">BOOKING FOR JULY</p>
-              <span class="separator-enquire">|</span>
-              <p class="scrolling-content-enquire">BOOKING FOR JULY</p>
-              <span class="separator-enquire">|</span>
-              <p class="scrolling-content-enquire">BOOKING FOR JULY</p>
-              <span class="separator-enquire">|</span>
-              <p class="scrolling-content-enquire">BOOKING FOR JULY</p>
-              <span class="separator-enquire">|</span>
-              <p class="scrolling-content-enquire">BOOKING FOR JULY</p>
-            </div>
-          </div>
+	<section id="photo-section">
+		<div id="photo">
+		</div>
+		<div class="scrolling-text-enquirenow">
+			<div class="text-container"><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><!-- Duplicate Content Starts Here --><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p><span class="separator">|</span><p class="scrolling-content">BOOKING FOR JULY</p></div>
+		</div>
 	</section>
+	<section class="brand-impact-section">
+    <div class="brand-impact-left">
+        <h2 class="brand-impact-title">
+            YOUR STORY IS<br>WHAT MAKES<br>THE DIFFERENCE
+        </h2>
+    </div>
+    <div class="brand-impact-right">
+        <p class="brand-impact-text">
+            We are inspired by authentic brands, and we want to help you bring yours to life. Reach out to me!
+        </p>
+        <hr class="brand-impact-line">
+        <p class="brand-impact-description">
+            Please <strong>fill out the form below</strong> so I can send you a personalized quote. I'll send the information via email, along with a link to schedule a call.
+        </p>
+    </div>
+</section>
 	<section id="enquirenow_form">
-		<h2 class="enquirenow_title">Let’s get started!</h2>
-		<p class="enquirenow_text">Hey there! Let’s get started! Please fill the form below so I can learn about you and your business. After this, I will contact you within 2 to 5 days (Costa Rica time zone).</p>
-		<p class="enquirenow_text">If you’re not looking for an inquiry and want to ask me something, please write me to my email! <a class="nvillalobos" href="mailto:nvillalobosc29@gmail.com">nvillalobosc29@gmail.com</a></p>
-		<p class="enquirenow_link"><a class="enquirenow_link" href="#">¿Hablas español? Clickea aquí para completar el formulario en español!</a></p>
 		<form>
 			<label class="enquirenow_label" for="firstName">Name*</label>
 			<div class="form-group">
@@ -45,6 +35,9 @@
 		
 			<label class="enquirenow_label" for="businessName">Business name*</label>
 			<input type="text" id="businessName" name="businessName" placeholder="Your Business Name">
+			
+			<label class="enquirenow_label" for="contact">Alternate contact method (e.g., your social media profile, phone number with country code if outside Costa Rica, etc.).*</label>
+			<input type="text" id="contact" name="contact" placeholder="Alternate Contact Method">
 		
 			<label class="enquirenow_label" for="location">Where are you based?*</label>
 			<input type="text" id="location" name="location" placeholder="Your Location">
@@ -55,82 +48,48 @@
 			<label class="enquirenow_label" for="aboutYou">Tell me about you and your business, please be detailed *</label>
 			<textarea id="aboutYou" name="aboutYou" rows="4" placeholder="Write about you and your business..."></textarea>
 		
-			<label class="enquirenow_label">What best describes your business?*</label>
+			<label class="enquirenow_label">What service are you looking for? *</label>
 			<div class="form-group-checkbox">
-				<label class ="enquirenow_label" for="entrepreneur">
-					<input type="checkbox" id="entrepreneur" name="businessType">
-					I'm an entrepreneur
+				<label class ="enquirenow_label" for="brightEssentials">
+					<input type="checkbox" id="brightEssentials" name="service">
+					Branding
 				</label>
 
-				<label class ="enquirenow_label" for="smallBusiness">
-					<input type="checkbox" id="smallBusiness" name="businessType">
-					I have a small business
+				<label class ="enquirenow_label" for="collaterals">
+					<input type="checkbox" id="collaterals" name="service">
+					Brand Collaterals(business cards, templates, gifs, flyers, etc)
 				</label>
 
-				<label class ="enquirenow_label" for="bigCompany">
-					<input type="checkbox" id="bigCompany" name="businessType">
-					We're a big company
+				<label class ="enquirenow_label" for="packaging">
+					<input type="checkbox" id="packaging" name="service">
+					Packaging(boxes, labels, etc)
+				</label>
+
+				<label class ="enquirenow_label" for="webDesign">
+					<input type="checkbox" id="webDesign" name="service">
+					Web Design & Development
+				</label>
+
+				<label class ="enquirenow_label" for="photography">
+					<input type="checkbox" id="photography" name="service">
+					Photography
+				</label>
+
+				<label class ="enquirenow_label" for="printableAssets">
+					<input type="checkbox" id="printableAssets" name="service">
+					Printable assets (business card, menus, posters...)
+				</label>
+
+				<label class ="enquirenow_label" for="socials">
+					<input type="checkbox" id="socials" name="service">
+					Socials Media Management
+				</label>
+
+				<label class ="enquirenow_label" for="other">
+					<input type="checkbox" id="other" name="service">
+					Other
 				</label>
 			</div>
-
-		<label class="enquirenow_label">What service are you looking for? *</label>
-		<div class="form-group-checkbox">
-			<label class ="enquirenow_label" for="coolBasic">
-				<input type="checkbox" id="coolBasic" name="service">
-				Cool Basic branding package
-			</label>
-
-			<label class ="enquirenow_label" for="brightEssentials">
-				<input type="checkbox" id="brightEssentials" name="service">
-				Bright Essentials branding package
-			</label>
-
-			<label class ="enquirenow_label" for="powerPremium">
-				<input type="checkbox" id="powerPremium" name="service">
-				Power Premium branding package
-			</label>
-
-			<label class ="enquirenow_label" for="startUp">
-				<input type="checkbox" id="startUp" name="service">
-				Start Up branding package
-			</label>
-
-			<label class ="enquirenow_label" for="selfManaged">
-				<input type="checkbox" id="selfManaged" name="service">
-				Self-managed social media
-			</label>
-
-			<label class ="enquirenow_label" for="packaging">
-				<input type="checkbox" id="packaging" name="service">
-				Packaging (boxes, labels...)
-			</label>
-
-			<label class ="enquirenow_label" for="printableAssets">
-				<input type="checkbox" id="printableAssets" name="service">
-				Printable assets (business card, menus, posters...)
-			</label>
-
-			<label class ="enquirenow_label" for="socials">
-				<input type="checkbox" id="socials" name="service">
-				Socials (social media templates, website design...)
-			</label>
-
-			<label class ="enquirenow_label" for="photography">
-				<input type="checkbox" id="photography" name="service">
-				Photography
-			</label>
-
-			<label class ="enquirenow_label" for="motionGraphics">
-				<input type="checkbox" id="motionGraphics" name="service">
-				Motion Graphics
-			</label>
-
-			<label class ="enquirenow_label" for="other">
-				<input type="checkbox" id="other" name="service">
-				Other
-			</label>
-		</div>
-		
 		
 			<label class="enquirenow_label" for="budget">What's your budget for this project? ($USD) *</label>
 			<input type="text" id="budget" name="budget" placeholder="Your Budget in USD">
