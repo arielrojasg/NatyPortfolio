3dafcd9445a30ba14d4ae6a6ce1fde644c2c0ced
Ariel Rojas <arojas@arielmac.local>
2024-11-17 17:11:06 -0600

    changes to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,16 @@
-// src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../views/home.vue'; // Adjust these imports to match your views
+import Home from '../views/home.vue';
+import About from '../views/about.vue';
+import Services from '../views/services.vue';
+import MyWork from '../views/mywork.vue';
+import EnquireNow from '../views/enquirenow.vue';
 
 const routes = [
   { path: '/', component: Home },
+  { path: '/about.html', component: About },
+  { path: '/services.html', component: Services },
+  { path: '/mywork.html', component: MyWork },
+  { path: '/enquirenow.html', component: EnquireNow },
 ];
 
 const router = createRouter({
