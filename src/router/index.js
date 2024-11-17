6cee01ece434b3b6fff50c291c52c1205079bbf6
Ariel Rojas <arojas@arielmac.local>
2024-11-17 17:07:38 -0600

    added CNAME and changes to router

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,13 @@
 // src/router/index.js
-import { createRouter, createWebHashHistory } from 'vue-router'; // Switch to createWebHashHistory if using hash mode
+import { createRouter, createWebHistory } from 'vue-router';
 import Home from '../views/home.vue'; // Adjust these imports to match your views
-import About from '../views/about.vue';
-import Services from '../views/services.vue';
-import EnquireNow from '../views/enquirenow.vue';
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/about', component: About },
-  { path: '/services', component: Services },
-  { path: '/enquirenow', component: EnquireNow },
 ];
 
 const router = createRouter({
-  // Use createWebHashHistory() for hash mode to prevent 404s on GitHub Pages
-  history: createWebHashHistory(), // Change to createWebHistory() if you prefer not using hash mode (requires server-side support)
+  history: createWebHistory(),
   routes,
 });
 
--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: '/', 
   plugins: [
-    vue() // Only the Vue plugin
+    vue()
   ],
   resolve: {
     alias: {
