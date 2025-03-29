f68b0ad72e46a1ffc14bd5fabcd606c2f881f57a
Ariel Rojas <arojas@arielmac.local>
2025-03-29 17:20:34 -0600

    added prerender from vite

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,24 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import prerender from 'vite-plugin-prerender'
 import path from 'path'
 
-export default defineConfig({
-  base: '/', 
-  plugins: [
-    vue(),
-    prerender({
-      staticDir: 'dist',
-      routes: ['/', '/about', '/services','/mywork','/enquirenow'], // add your real routes
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./public"), // General alias for src
-      "@images": path.resolve(__dirname, "./public/img") // Alias for images
-    }
-  }
-})
+export default defineConfig(async () => {
+  const { default: prerender } = await import('vite-plugin-prerender');
+
+  return {
+    base: '/',
+    plugins: [
+      vue(),
+      prerender({
+        staticDir: 'dist',
+        routes: ['/', '/about', '/services', '/mywork','/enquirenow'], // Add your actual routes here
+      }),
+    ],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './public'),
+        '@images': path.resolve(__dirname, './public/img'),
+      },
+    },
+  };
+});
