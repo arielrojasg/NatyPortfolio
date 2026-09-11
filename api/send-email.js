fe96ab1356141bc0983af82d636e74bc440b2a2a
Ariel Rojas <arojas@arielmac.local>
2026-09-10 23:31:28 -0600

    removed second email

--- api/send-email.js
+++ api/send-email.js
@@ -204,10 +204,13 @@ export default async function handler(req, res) {
 
       const scheduledAt = new Date(Date.now() + 2 * 60 * 1000).toISOString();
 
-      const [response1, response2] = await Promise.all([
-        axios.post("https://api.brevo.com/v3/smtp/email", { ...emailConfig, htmlContent: htmlContent }, axiosConfig),
-        //axios.post("https://api.brevo.com/v3/smtp/email", { ...emailConfigResponse, htmlContent: htmlResponse, scheduledAt, replyTo: { email: "nvillalobosc29@gmail.com", name: "Natalia Villalobos" } }, axiosConfig),
-      ]);
+      // const [response1, response2] = await Promise.all([
+      //   axios.post("https://api.brevo.com/v3/smtp/email", { ...emailConfig, htmlContent: htmlContent }, axiosConfig),
+      //   axios.post("https://api.brevo.com/v3/smtp/email", { ...emailConfigResponse, htmlContent: htmlResponse, scheduledAt, replyTo: { email: "nvillalobosc29@gmail.com", name: "Natalia Villalobos" } }, axiosConfig),
+      // ]);
+
+      const response1 = await axios.post("https://api.brevo.com/v3/smtp/email",{ ...emailConfig, htmlContent: htmlContent },axiosConfig
+);
 
       res.writeHead(200, { "Content-Type": "application/json", ...corsHeaders });
       res.end(JSON.stringify({ success: true, message: "Email sent, follow-up scheduled in 2 minutes", response: [response1.data, response2.data] }));
